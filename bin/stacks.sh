3e1c88ad8755b4ed6405e69d1b683397806fb1e5
techcto <smoore@solodev.com>
2019-12-18 12:33:04 -0500

    merge

---
+++ bin/stacks.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+#Params
+CLUSTER=1
+MEGACLUSTER=1
+DEPLOYMENT=0
+MPDEPLOYMENT=0
+
+if [ $CLUSTER == 1 ]; then
+
+echo "Create AWS EKS Cluster"
+echo $(aws s3 cp s3://build-secure/params/amazon-eks.json - ) > amazon-eks.json
+aws cloudformation create-stack --disable-rollback --stack-name eks-tmp-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --parameters file:///${CODEBUILD_SRC_DIR}/amazon-eks.json \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/amazon-eks.yaml \
+    # --notification-arns $NOTIFICATION_ARN
+
+fi
+
+if [ $MEGACLUSTER == 1 ]; then
+
+echo "Create AWS EKS Mega Cluster"
+echo $(aws s3 cp s3://build-secure/params/amazon-eks-mega.json - ) > amazon-eks.json
+aws cloudformation create-stack --disable-rollback --stack-name eksmega-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --parameters file:///${CODEBUILD_SRC_DIR}/amazon-eks.json \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/amazon-eks.yaml \
+    # --notification-arns $NOTIFICATION_ARN
+
+fi
+
+if [ $DEPLOYMENT == 1 ]; then
+
+echo "Install Solodev DCX"
+echo $(aws s3 cp s3://build-secure/params/solodev-dcx.json - ) > solodev-dcx.json
+aws cloudformation create-stack --disable-rollback --stack-name solo-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --parameters file:///${CODEBUILD_SRC_DIR}/solodev-dcx.json \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/solodev-dcx.yaml \
+    # --notification-arns $NOTIFICATION_ARN
+
+fi
+
+if [ $MPDEPLOYMENT == 1 ]; then
+
+echo "Install Solodev DCX for Marketplace"
+echo $(aws s3 cp s3://build-secure/params/solodev-dcx.json - ) > solodev-dcx.json
+aws cloudformation create-stack --disable-rollback --stack-name solo-${DATE}mp --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --parameters file:///${CODEBUILD_SRC_DIR}/solodev-dcx.json \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/solodev-dcx-aws.yaml \
+    # --notification-arns $NOTIFICATION_ARN
+
+fi

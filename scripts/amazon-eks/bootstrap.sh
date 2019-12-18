6805357a758a0b06c8cba040df85a2686a3eaab2
techcto <smoore@solodev.com>
2019-12-18 15:20:33 -0500

    ..

--- scripts/amazon-eks/bootstrap.sh
+++ scripts/amazon-eks/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#https://aws-quickstart.s3.amazonaws.com/quickstart-amazon-eks/scripts/amazon-eks/bootstrap.sh
 
 set -o pipefail
 set -o nounset
@@ -21,9 +20,6 @@ function print_help {
     echo "--b64-cluster-ca The base64 encoded cluster CA content. Only valid when used with --apiserver-endpoint. Bypasses calling \"aws eks describe-cluster\""
     echo "--apiserver-endpoint The EKS cluster API Server endpoint. Only valid when used with --b64-cluster-ca. Bypasses calling \"aws eks describe-cluster\""
     echo "--kubelet-extra-args Extra arguments to add to the kubelet. Useful for adding labels or taints."
-    echo "--enable-docker-bridge Restores the docker default bridge network. (default: false)"
-    echo "--aws-api-retry-attempts Number of retry attempts for AWS API call (DescribeCluster) (default: 3)"
-    echo "--docker-config-json The contents of the /etc/docker/daemon.json file. Useful if you want a custom config differing from the default one in the AMI"
     echo "--solodev-network Enable Solodev network settings (default: false)."
 }
 
@@ -56,31 +52,6 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
-        --enable-docker-bridge)
-            ENABLE_DOCKER_BRIDGE=$2
-            shift
-            shift
-            ;;
-        --aws-api-retry-attempts)
-            API_RETRY_ATTEMPTS=$2
-            shift
-            shift
-            ;;
-        --docker-config-json)
-            DOCKER_CONFIG_JSON=$2
-            shift
-            shift
-            ;;
-        --pause-container-account)
-            PAUSE_CONTAINER_ACCOUNT=$2
-            shift
-            shift
-            ;;
-        --pause-container-version)
-            PAUSE_CONTAINER_VERSION=$2
-            shift
-            shift
-            ;;
         --solodev-network)
             SOLODEV_NETWORK=$2
             shift
@@ -99,26 +70,10 @@ CLUSTER_NAME="$1"
 set -u
 
 USE_MAX_PODS="${USE_MAX_PODS:-true}"
-SOLODEV_NETWORK="${SOLODEV_NETWORK:-false}"
 B64_CLUSTER_CA="${B64_CLUSTER_CA:-}"
 APISERVER_ENDPOINT="${APISERVER_ENDPOINT:-}"
 KUBELET_EXTRA_ARGS="${KUBELET_EXTRA_ARGS:-}"
-ENABLE_DOCKER_BRIDGE="${ENABLE_DOCKER_BRIDGE:-false}"
-API_RETRY_ATTEMPTS="${API_RETRY_ATTEMPTS:-3}"
-DOCKER_CONFIG_JSON="${DOCKER_CONFIG_JSON:-}"
-PAUSE_CONTAINER_VERSION="${PAUSE_CONTAINER_VERSION:-3.1}"
-
-function get_pause_container_account_for_region () {
-    local region="$1"
-    case "${region}" in
-    ap-east-1)
-        echo "${PAUSE_CONTAINER_ACCOUNT:-800184023465}";;
-    me-south-1)
-        echo "${PAUSE_CONTAINER_ACCOUNT:-558608220178}";;
-    *)
-        echo "${PAUSE_CONTAINER_ACCOUNT:-602401143452}";;
-    esac
-}
+SOLODEV_NETWORK="${SOLODEV_NETWORK:-false}"
 
 if [ -z "$CLUSTER_NAME" ]; then
     echo "CLUSTER_NAME is not defined"
@@ -128,16 +83,6 @@ fi
 ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 AWS_DEFAULT_REGION=$(echo $ZONE | awk '{print substr($0, 1, length($0)-1)}')
 
-MACHINE=$(uname -m)
-if [ "$MACHINE" == "x86_64" ]; then
-    ARCH="amd64"
-elif [ "$MACHINE" == "aarch64" ]; then
-    ARCH="arm64"
-else
-    echo "Unknown machine architecture '$MACHINE'" >&2
-    exit 1
-fi
-
 ### kubelet kubeconfig
 
 CA_CERTIFICATE_DIRECTORY=/etc/kubernetes/pki
@@ -145,32 +90,11 @@ CA_CERTIFICATE_FILE_PATH=$CA_CERTIFICATE_DIRECTORY/ca.crt
 mkdir -p $CA_CERTIFICATE_DIRECTORY
 if [[ -z "${B64_CLUSTER_CA}" ]] && [[ -z "${APISERVER_ENDPOINT}" ]]; then
     DESCRIBE_CLUSTER_RESULT="/tmp/describe_cluster_result.txt"
-    rc=0
-    # Retry the DescribleCluster API for API_RETRY_ATTEMPTS
-    for attempt in `seq 0 $API_RETRY_ATTEMPTS`; do
-        if [[ $attempt -gt 0 ]]; then
-            echo "Attempt $attempt of $API_RETRY_ATTEMPTS"
-        fi
-
-        aws eks wait cluster-active \
-            --region=${AWS_DEFAULT_REGION} \
-            --name=${CLUSTER_NAME}
-
-        aws eks describe-cluster \
-            --region=${AWS_DEFAULT_REGION} \
-            --name=${CLUSTER_NAME} \
-            --output=text \
-            --query 'cluster.{certificateAuthorityData: certificateAuthority.data, endpoint: endpoint}' > $DESCRIBE_CLUSTER_RESULT || rc=$?
-        if [[ $rc -eq 0 ]]; then
-            break
-        fi
-        if [[ $attempt -eq $API_RETRY_ATTEMPTS ]]; then
-            exit $rc
-        fi
-        jitter=$((1 + RANDOM % 10))
-        sleep_sec="$(( $(( 5 << $((1+$attempt)) )) + $jitter))"
-        sleep $sleep_sec
-    done
+    aws eks describe-cluster \
+        --region=${AWS_DEFAULT_REGION} \
+        --name=${CLUSTER_NAME} \
+        --output=text \
+        --query 'cluster.{certificateAuthorityData: certificateAuthority.data, endpoint: endpoint}' > $DESCRIBE_CLUSTER_RESULT
     B64_CLUSTER_CA=$(cat $DESCRIBE_CLUSTER_RESULT | awk '{print $1}')
     APISERVER_ENDPOINT=$(cat $DESCRIBE_CLUSTER_RESULT | awk '{print $2}')
 fi
@@ -178,18 +102,25 @@ fi
 echo $B64_CLUSTER_CA | base64 -d > $CA_CERTIFICATE_FILE_PATH
 
 sed -i s,CLUSTER_NAME,$CLUSTER_NAME,g /var/lib/kubelet/kubeconfig
-sed -i s,MASTER_ENDPOINT,$APISERVER_ENDPOINT,g /var/lib/kubelet/kubeconfig
+kubectl config \
+    --kubeconfig /var/lib/kubelet/kubeconfig \
+    set-cluster \
+    kubernetes \
+    --certificate-authority=/etc/kubernetes/pki/ca.crt \
+    --server=$APISERVER_ENDPOINT
+
 ### kubelet.service configuration
 
-MAC=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ -s | head -n 1 | sed 's/\/$//')
-TEN_RANGE=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/vpc-ipv4-cidr-blocks | grep -c '^10\..*' || true )
+MAC=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ -s | head -n 1)
+CIDRS=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/vpc-ipv4-cidr-blocks)
+TEN_RANGE=$(echo $CIDRS | grep -c '^10\..*' || true )
 DNS_CLUSTER_IP=10.100.0.10
 if [[ "$TEN_RANGE" != "0" ]] ; then
     DNS_CLUSTER_IP=172.20.0.10;
 fi
 
 KUBELET_CONFIG=/etc/kubernetes/kubelet/kubelet-config.json
-echo "$(jq ".clusterDNS=[\"$DNS_CLUSTER_IP\"]" $KUBELET_CONFIG)" > $KUBELET_CONFIG
+echo "$(jq .clusterDNS=[\"$DNS_CLUSTER_IP\"] $KUBELET_CONFIG)" > $KUBELET_CONFIG
 
 INTERNAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 INSTANCE_TYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
@@ -263,7 +194,7 @@ KUBELET_EXTRA_ARGS="${KUBELET_EXTRA_ARGS} --feature-gates CSINodeInfo=true,CSIDr
 
 cat <<EOF > /etc/systemd/system/kubelet.service.d/10-kubelet-args.conf
 [Service]
-Environment='KUBELET_ARGS=--node-ip=$INTERNAL_IP --pod-infra-container-image=$(get_pause_container_account_for_region "${AWS_DEFAULT_REGION}").dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/eks/pause-${ARCH}:$PAUSE_CONTAINER_VERSION'
+Environment='KUBELET_ARGS=--node-ip=$INTERNAL_IP --pod-infra-container-image=602401143452.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/eks/pause-amd64:3.1'
 EOF
 
 if [[ -n "$KUBELET_EXTRA_ARGS" ]]; then
@@ -276,19 +207,6 @@ fi
 # if [[ "$SOLODEV_NETWORK" = "true" ]]; then
 
 # fi
-
-# Replace with custom docker config contents.
-if [[ -n "$DOCKER_CONFIG_JSON" ]]; then
-    echo "$DOCKER_CONFIG_JSON" > /etc/docker/daemon.json
-    systemctl restart docker
-fi
-
-if [[ "$ENABLE_DOCKER_BRIDGE" = "true" ]]; then
-    # Enabling the docker bridge network. We have to disable live-restore as it
-    # prevents docker from recreating the default bridge network on restart
-    echo "$(jq '.bridge="docker0" | ."live-restore"=false' /etc/docker/daemon.json)" > /etc/docker/daemon.json
-    systemctl restart docker
-fi
 
 systemctl daemon-reload
 systemctl enable kubelet

8436d0276d9d80b080b2f7f403f2348f444c6986
techcto <smoore@solodev.com>
2020-02-13 08:14:29 -0500

    update template location

--- ci/bin/stacks.sh
+++ ci/bin/stacks.sh
@@ -12,7 +12,7 @@ echo "Create AWS EKS Cluster"
 echo $(aws s3 cp s3://build-secure/params/amazon-eks.json - ) > amazon-eks.json
 aws cloudformation create-stack --disable-rollback --stack-name eks-tmp-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameters file:///${CODEBUILD_SRC_DIR}/amazon-eks.json \
-    --template-url https://s3.amazonaws.com/quickstart-solodev-eks/templates/solodev-eks.yaml \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/templates/solodev-eks.yaml \
     # --notification-arns $NOTIFICATION_ARN
 
 fi
@@ -23,7 +23,7 @@ echo "Create AWS EKS Mega Cluster"
 echo $(aws s3 cp s3://build-secure/params/amazon-eks-mega.json - ) > amazon-eks.json
 aws cloudformation create-stack --disable-rollback --stack-name eksmega-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameters file:///${CODEBUILD_SRC_DIR}/amazon-eks.json \
-    --template-url https://s3.amazonaws.com/quickstart-solodev-eks/templates/solodev-eks.yaml \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/templates/solodev-eks.yaml \
     # --notification-arns $NOTIFICATION_ARN
 
 fi
@@ -34,7 +34,7 @@ echo "Install Solodev DCX"
 echo $(aws s3 cp s3://build-secure/params/solodev-dcx.json - ) > solodev-dcx.json
 aws cloudformation create-stack --disable-rollback --stack-name solo-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameters file:///${CODEBUILD_SRC_DIR}/solodev-dcx.json \
-    --template-url https://s3.amazonaws.com/quickstart-solodev-eks/templates/solodev-cms.yaml \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/templates/solodev-cms.yaml \
     # --notification-arns $NOTIFICATION_ARN
 
 fi
@@ -45,7 +45,7 @@ echo "Install Solodev DCX for Marketplace"
 echo $(aws s3 cp s3://build-secure/params/solodev-dcx.json - ) > solodev-dcx.json
 aws cloudformation create-stack --disable-rollback --stack-name solo-${DATE}mp --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameters file:///${CODEBUILD_SRC_DIR}/solodev-dcx.json \
-    --template-url https://s3.amazonaws.com/quickstart-solodev-eks/templates/solodev-cms-aws.yaml \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/templates/solodev-cms-aws.yaml \
     # --notification-arns $NOTIFICATION_ARN
 
 fi

cb9d1f7c76148d43ac1fc2e6592f3b693f0ec290
techcto <smoore@solodev.com>
2020-04-16 08:17:25 -0400

    taskcat setup

--- cmd.sh
+++ cmd.sh
@@ -4,13 +4,22 @@ args=("$@")
 
 init(){
     git remote add upstream https://github.com/aws-quickstart/quickstart-amazon-eks.git
+    git submodule add https://github.com/aws-quickstart/quickstart-amazon-eks-cluster-resource-provider.git ./functions/source/EksClusterResource
 }
 
-update() {
+pullMaster() {
     git pull https://github.com/aws-quickstart/quickstart-amazon-eks.git master --allow-unrelated-histories
     git fetch upstream
     git checkout master
     git merge upstream/master
+}
+
+update(){
+    git submodule update
+}
+
+build(){
+    taskcat test run
 }
 
 $*

e76f11f73481417ab18207a3ccd191afeac6d98e
techcto <smoore@solodev.com>
2020-04-20 16:21:57 -0400

    ..

--- ci/bin/stacks.sh
+++ ci/bin/stacks.sh
@@ -10,7 +10,7 @@ echo "Create AWS EKS Cluster"
 echo $(aws s3 cp s3://build-secure/params/amazon-eks.json - ) > amazon-eks.json
 aws cloudformation create-stack --disable-rollback --stack-name eks-tmp-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameters file:///${CODEBUILD_SRC_DIR}/amazon-eks.json \
-    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/templates/amazon-eks-master.template.yaml \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/templates/amazon-eks-master-existing-vpc.template.yaml \
 
 fi
 

f74be8da1509fced9079b29450e1076694aa8fec
techcto <smoore@solodev.com>
2020-04-21 09:20:40 -0400

    ..

--- ci/bin/stacks.sh
+++ ci/bin/stacks.sh
@@ -2,7 +2,7 @@
 
 #Params
 CLUSTER=1
-MEGACLUSTER=0
+MEGACLUSTER=1
 
 if [ $CLUSTER == 1 ]; then
 
@@ -20,6 +20,6 @@ echo "Create AWS EKS Mega Cluster"
 echo $(aws s3 cp s3://build-secure/params/amazon-eks-mega.json - ) > amazon-eks.json
 aws cloudformation create-stack --disable-rollback --stack-name eksmega-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameters file:///${CODEBUILD_SRC_DIR}/amazon-eks.json \
-    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/templates/solodev-eks-master.template.yaml \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/templates/solodev-eks-master-existing-vpc.template.yaml \
 
 fi

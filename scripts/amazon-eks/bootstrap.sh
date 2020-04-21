8c107fc7ad624ff775a0e9b2a2f771115f7c9144
techcto <smoore@solodev.com>
2020-04-21 07:40:34 -0400

    weave CNI

--- scripts/amazon-eks/bootstrap.sh
+++ scripts/amazon-eks/bootstrap.sh
@@ -20,6 +20,7 @@ function print_help {
     echo "--b64-cluster-ca The base64 encoded cluster CA content. Only valid when used with --apiserver-endpoint. Bypasses calling \"aws eks describe-cluster\""
     echo "--apiserver-endpoint The EKS cluster API Server endpoint. Only valid when used with --b64-cluster-ca. Bypasses calling \"aws eks describe-cluster\""
     echo "--kubelet-extra-args Extra arguments to add to the kubelet. Useful for adding labels or taints."
+    echo "--enable-weave Enable Weave CNI (default: false)."
 }
 
 POSITIONAL=()
@@ -51,6 +52,11 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        --enable-weave)
+            SOLODEV_NETWORK=$2
+            shift
+            shift
+            ;;
         *)    # unknown option
             POSITIONAL+=("$1") # save it in an array for later
             shift # past argument
@@ -67,6 +73,7 @@ USE_MAX_PODS="${USE_MAX_PODS:-true}"
 B64_CLUSTER_CA="${B64_CLUSTER_CA:-}"
 APISERVER_ENDPOINT="${APISERVER_ENDPOINT:-}"
 KUBELET_EXTRA_ARGS="${KUBELET_EXTRA_ARGS:-}"
+ENABLE_WEAVE="${ENABLE_WEAVE:-false}"
 
 if [ -z "$CLUSTER_NAME" ]; then
     echo "CLUSTER_NAME is not defined"
@@ -123,12 +130,64 @@ if [[ "$USE_MAX_PODS" = "true" ]]; then
     set +o pipefail
     MAX_PODS=$(grep $INSTANCE_TYPE $MAX_PODS_FILE | awk '{print $2}')
     set -o pipefail
+    if [[ "$ENABLE_WEAVE" = "true" ]]; then
+        MAX_PODS=$(($MAX_PODS*5))
+    fi
     if [[ -n "$MAX_PODS" ]]; then
         echo "$(jq .maxPods=$MAX_PODS $KUBELET_CONFIG)" > $KUBELET_CONFIG
     else
         echo "No entry for $INSTANCE_TYPE in $MAX_PODS_FILE. Not setting max pods for kubelet"
     fi
 fi
+
+if [[ $(ec2-metadata --instance-type) =~ 'large' ]]; then
+    mem_reserved=1Gi
+else
+    mem_reserved=0.5Gi
+fi
+
+## Kube network optimisation.
+cat <<EOF > /etc/sysctl.d/99-kube-net.conf
+# Have a larger connection range available
+net.ipv4.ip_local_port_range=1024 65000
+ 
+# Reuse closed sockets faster
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_fin_timeout=15
+ 
+# The maximum number of "backlogged sockets".  Default is 128.
+net.core.somaxconn=4096
+net.core.netdev_max_backlog=4096
+ 
+# 16MB per socket - which sounds like a lot,
+# but will virtually never consume that much.
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+ 
+# Various network tunables
+net.ipv4.tcp_max_syn_backlog=20480
+net.ipv4.tcp_max_tw_buckets=400000
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_rmem=4096 87380 16777216
+net.ipv4.tcp_syn_retries=2
+net.ipv4.tcp_synack_retries=2
+net.ipv4.tcp_wmem=4096 65536 16777216
+#vm.min_free_kbytes=65536
+ 
+# Connection tracking to prevent dropped connections (usually issue on LBs)
+net.netfilter.nf_conntrack_max=262144
+net.ipv4.netfilter.ip_conntrack_generic_timeout=120
+net.netfilter.nf_conntrack_tcp_timeout_established=86400
+ 
+# ARP cache settings for a highly loaded docker swarm
+net.ipv4.neigh.default.gc_thresh1=8096
+net.ipv4.neigh.default.gc_thresh2=12288
+net.ipv4.neigh.default.gc_thresh3=16384
+EOF
+ 
+systemctl restart systemd-sysctl.service
+
+KUBELET_EXTRA_ARGS="${KUBELET_EXTRA_ARGS} --feature-gates CSINodeInfo=true,CSIDriverRegistry=true,CSIBlockVolume=true --kube-reserved cpu=250m,memory=${mem_reserved},ephemeral-storage=1Gi --system-reserved cpu=250m,memory=0.2Gi,ephemeral-storage=1Gi --eviction-hard memory.available<500Mi,nodefs.available<10%"
 
 cat <<EOF > /etc/systemd/system/kubelet.service.d/10-kubelet-args.conf
 [Service]
@@ -140,6 +199,10 @@ if [[ -n "$KUBELET_EXTRA_ARGS" ]]; then
 [Service]
 Environment='KUBELET_EXTRA_ARGS=$KUBELET_EXTRA_ARGS'
 EOF
+fi
+
+if [[ "$ENABLE_WEAVE" = "true" ]]; then
+    rm -Rf /etc/cni/net.d/10-aws.conflist /opt/cni/bin/aws-cni
 fi
 
 systemctl daemon-reload

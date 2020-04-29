93471543d1421d4cae2dae42d81f14c94153bbe2
techcto <smoore@solodev.com>
2020-04-29 16:34:21 -0400

    ..

--- functions/source/KubeManifest/lambda_function.py
+++ functions/source/KubeManifest/lambda_function.py
@@ -208,7 +208,10 @@ def enable_proxy(proxy_host, vpc_id):
 
 def enable_weave(cluster_name):
     logger.debug(run_command("kubectl delete ds aws-node -n kube-system"))
-    logger.debug(run_command("curl --location -o /tmp/weave-net.yaml \"https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')\""))
+    weave_cmd = (
+        """curl --location -o /tmp/weave-net.yaml "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')""""
+    )
+    logger.debug(run_command(weave_cmd)
     logger.debug(run_command("kubectl apply -f /tmp/weave-net.yaml"))
 
 def enable_marketplace(cluster_name, namespace):

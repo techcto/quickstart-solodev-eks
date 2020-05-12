d624f5ff551cb309f52abde7104d77c9793f0065
techcto <smoore@solodev.com>
2020-05-12 15:55:23 -0400

    ..

--- ci/bin/stacks.sh
+++ ci/bin/stacks.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #Params...
-OCLUSTER=1
+OCLUSTER=0
 CLUSTER=1
-MCLUSTER=1
+MCLUSTER=0
 
 if [ $OCLUSTER == 1 ]; then
 
@@ -20,7 +20,7 @@ echo "Create AWS EKS 1.15 Cluster"
 echo $(aws s3 cp s3://build-secure/params/aws-eks-115.json - ) > eks.json
 aws cloudformation create-stack --disable-rollback --stack-name tmp-eks-115-${CODEBUILD_BUILD_NUMBER} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameters file:///${CODEBUILD_SRC_DIR}/eks.json \
-    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/1.15/templates/amazon-eks-master-existing-vpc.template.yaml
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/1.15/templates/solodev-eks-master-existing-vpc.template.yaml
 fi
 
 if [ $MCLUSTER == 1 ]; then
